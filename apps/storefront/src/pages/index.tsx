f43b5bafb59fce4ffb5ff000d4bfd3a9909b8aed
jchen-vercel <jason.chen@vercel.com>
2022-08-05 10:22:27 -0400

    change on storefront app

--- apps/storefront/src/pages/index.tsx
+++ apps/storefront/src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Store() {
   log("Hey! This is Home.");
   return (
     <div>
-      <h1>Store</h1>
+      <h1>Store1</h1>
       <CounterButton />
       <NewTabLink href="https://turborepo.org/">
         Built With TurboRepo
